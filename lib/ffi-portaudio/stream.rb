fbcdcbf099b4adb411b561607aa1921707260ae0
nanki <nanki@dotswitch.net>
2011-09-25 07:20:39 +0900

    FIX: method(:process) was garbage-collected.

--- lib/ffi-portaudio/stream.rb
+++ lib/ffi-portaudio/stream.rb
@@ -4,7 +4,7 @@ class FFI::PortAudio::Stream
   def open(input, output, freq, frames=API::FramesPerBufferUnspecified, flags=API::NoFlag, userdata=nil)
     @input, @output, @freq, @frames, @flags, @userdata = input, output, freq, frames, flags, userdata
     @stream = FFI::Buffer.new :pointer
-    API.Pa_OpenStream(@stream, @input, @output, @freq, @frames, @flags, method(:process), @userdata)
+    API.Pa_OpenStream(@stream, @input, @output, @freq, @frames, @flags, @method = method(:process), @userdata)
   end
 
   def process(input, output, frameCount, timeInfo, statusFlags, userData);end
